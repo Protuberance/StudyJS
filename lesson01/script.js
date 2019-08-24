add43d5d057f5d0c54f079a87ed20160015a5685
Andrey Tabachenkov <booomser13@gmail.com>
2019-08-24 21:43:30 +0300

    update to lesson02

--- lesson01/script.js
+++ lesson01/script.js
@@ -1,9 +1,19 @@
-const money = 25,
-    income = 2,
-    addExpenses = 10,
-    deposit = 20,
-    mission = "Save the money",
-    period = 3;
+const money = 100000,
+    income = 'freelance',
+    addExpenses = 'Cinema, Travels, Extreme Activities',
+    deposit = true,
+    mission = 5000000,
+    period = 10;
 
-alert('We have ' + money + ' of money');
-console.log(mission);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log('Период ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses.split(', ').forEach(expense => {
+    console.log(expense.toLocaleLowerCase());
+});
