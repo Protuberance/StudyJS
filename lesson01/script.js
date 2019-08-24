3032f77ae21ab2f2bda0b88b7b4abea248f116d2
Andrey Tabachenkov <booomser13@gmail.com>
2019-08-24 17:58:33 +0300

    git init

---
+++ lesson01/script.js
@@ -0,0 +1,10 @@
+const money = 25,
+    income = 2,
+    addExpenses = 10,
+    deposit = 20,
+    mission = "Save the money",
+    period = 3;
+
+alert('We have ' + money + ' of money');
+
+console.log(mission);
