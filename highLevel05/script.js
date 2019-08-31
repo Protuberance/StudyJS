934e587353d4aba6c36a2597b705103ff90448be
Andrey Tabachenkov <booomser13@gmail.com>
2019-08-31 20:03:56 +0300

    highlevel06

--- highLevel05/script.js
+++ highLevel05/script.js
@@ -1,23 +1,15 @@
 'use strict'
 
-let arr = [];
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+var date = new Date();
 
-arr[0] = '245674';
-arr[1] = '475644';
+for (let i = 0; i < week.length; i++) {
+    if (i == date.getDay()) {
+        console.log(week[i].bold());
+    } else if (i == 0 || i == 6) {
+        console.log(week[i].italics());
 
-for (let i = 2; i < 7; i++) {
-    arr[i] = Math.floor((Math.random() * 10000)).toString();
-}
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i].substr(0, 1) === '2' || arr[i].substr(0, 1) === '4')
-        console.log(arr[i]);
-}
-
-let simpledigitals = ['2', '3', '5', '7', '11', '13', '17', '19', '23', '29', '31', '37', '41', '43', '47', '53', '59', '61', '67', '71', '73', '79', '83', '89', '97'];
-
-for (let i = 0; i < simpledigitals.length; i++) {
-    console.log(simpledigitals[i] +
-        ' - Делители этого числа: 1 и ' + simpledigitals[i]);;
+    } else
+        console.log(week[i]);
 
 }
