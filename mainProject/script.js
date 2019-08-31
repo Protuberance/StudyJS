38bb19fcf92e88e8c602f8cfe228ce6fa0ba7835
Andrey Tabachenkov <booomser13@gmail.com>
2019-08-31 17:09:58 +0300

    mainProjectLesson07

--- mainProject/script.js
+++ mainProject/script.js
@@ -18,13 +18,27 @@ let appData = {
     addIncome: [],
     addExpenses: [],
     deposit: false,
+    percentDeposit: 0,
+    moneyDeposit: 0,
     mission: 5000000,
     period: 5,
     expenses: {},
     asking: function () {
+        if (confirm('Есть ли у вас дополнительный заработок?')) {
+            let itemIncome,
+                cashIncome;
+            do {
+                itemIncome = prompt('Какой у вас есть дополнительный заработок',
+                    'Такси');
+                cashIncome = prompt('Сколько вы зарабатываете?',
+                    '10000');
+            }
+            while (IsNotOrEmpty(itemIncome) || IsZeroOrNotNumber(cashIncome));
+            this.income[itemIncome] = cashIncome;
+        }
         let succes = false;
         while (!succes) {
-            let response = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'clothes', 'cinema');
+            let response = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'clothes\, cinema');
             if (response !== '' && response !== null && response !== undefined) {
                 appData.addExpenses = response.split(', ');
                 succes = true;
@@ -36,13 +50,13 @@ let appData = {
         let expenseValue,
             expensesName;
         for (let i = 0; i < 2; i++) {
-            expensesName = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'iiieeeyhhaaaa');
             do {
-                expenseValue = prompt('Во сколько это обойдется?', 35000);
+                expensesName = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'iiieeeyhhaaaa');
+                expenseValue = +prompt('Во сколько это обойдется?', 35000);
+                console.log(expenseValue);
             }
-            while (expenseValue === null || expenseValue === '' || isNaN(expenseValue))
-            if (this.expenses[expensesName] === undefined)
-                this.expenses[expensesName] = expenseValue;
+            while (IsZeroOrNotNumber(expenseValue) || IsNotOrEmpty(expensesName))
+            this.expenses[expensesName] = expenseValue;
         }
     },
     getExpensesMonth: function () {
@@ -70,16 +84,53 @@ let appData = {
             console.log('Низкий уровень дохода');
         else if (this.budgetDay <= 0)
             console.log('Что то пошло не так');
+    },
+    getInfoDepost: function () {
+        if (this.deposit) {
+            do {
+                this.percentDeposit = prompt('Какой годовой процент?', '10');
+                this.moneyDeposit = prompt('Какая сумма заложена?', '10000');
+            }
+            while (IsZeroOrNotNumber(this.percentDeposit) || IsZeroOrNotNumber(this.moneyDeposit))
+        }
+    },
+    calcSavedMoney: function () {
+        return this.budgetMonth * this.period;
     }
 }
 appData.asking();
 appData.getBudget();
 appData.getExpensesMonth();
-// console.log("Сумма ваших накоплений за месяц равна " + accumulatedMonth);
 console.log('Сумма расходов за месяц равна ' + appData.expensesMonth);
 console.log(appData.getTargetMonth());
 appData.getStatusIncome();
 
 for (let key in appData) {
     console.log(key + ' : ' + appData[key]);
+}
+
+
+function getExpensesTxt() {
+    let array = new Array(appData.addExpenses.length),
+        text;
+    for (let i = 0; i < appData.addExpenses.length; i++) {
+        let row = appData.addExpenses[i];
+        let firstLetter = row.slice(0, 1);
+        array[i] = row.replace(firstLetter, firstLetter.toUpperCase());
+    }
+    return array.join(', ');
+}
+
+console.log(getExpensesTxt());
+
+function IsNotOrEmpty(str) {
+    if (str === '' || str === null || str === undefined)
+        return true;
+    else return false;
+}
+
+function IsZeroOrNotNumber(isNumber) {
+    if (isNumber === 0 || isNumber === null || isNaN(isNumber) || isNumber === undefined)
+        return true;
+    else return false;
 }
