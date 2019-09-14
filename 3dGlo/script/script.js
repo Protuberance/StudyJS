8a91c44056567fa4774e59acf6a5af3f247c8786
Andrey Tabachenkov <booomser13@gmail.com>
2019-09-14 11:57:20 +0300

    mainprojectLesson12 general tasks

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -1,6 +1,13 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict'
+    let menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        btnMenu = document.querySelector('.menu'),
+        menuItems = menu.querySelectorAll('ul>li'),
+        popup = document.querySelector('.popup');
 
+    console.log(navigator.userAgent);
+    //#region Timer
     function countTimer(deadLine) {
 
         let timerHours = document.querySelector('#timer-hours'),
@@ -39,8 +46,59 @@ window.addEventListener('DOMContentLoaded', function () {
             timerSeconds.textContent = getPrettyTime(timer.seconds);
         }
     }
+    countTimer('1 january 2020');
+    //#endregion
 
-    countTimer('11 september 2009');
+    //#region Menu
+    const toggleMenu = () => {
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        }
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach((element) => element.addEventListener('click', handlerMenu));
+    }
+    //#endregion
+    toggleMenu();
+
+    //#region popUp
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popUpBtns = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+
+        let animationIndex,
+            opacity = 0;
+        const openPopUp = () => {
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                popup.style.display = 'block';
+            } else {
+                popup.style.opacity = opacity;
+                popup.style.display = 'block';
+                animationIndex = requestAnimationFrame(animationClose);
+            }
+        };
+        const animationClose = () => {
+            animationIndex = requestAnimationFrame(animationClose);
+
+            popup.style.opacity = opacity;
+            opacity += 0.1;
+            if (opacity >= 1)
+                cancelAnimationFrame(animationIndex);
+        };
+        const closePopUp = () => {
+            opacity = 0;
+            popup.style.display = 'none';
+        };
+
+        popUpBtns.forEach((element) => {
+            element.addEventListener('click', openPopUp);
+        });
+        popUpClose.addEventListener('click', closePopUp);
+    }
+    togglePopUp();
+    //#endregion
 
     function getPrettyTime(stringTime) {
         let _stringTime = stringTime.toString();
@@ -48,4 +106,4 @@ window.addEventListener('DOMContentLoaded', function () {
             return '0' + _stringTime;
         else return _stringTime;
     }
-})
+});
