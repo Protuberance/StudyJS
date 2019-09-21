ad88f7eca0d1194f7c7cdf07a24b449560ddb147
Andrey Tabachenkov <booomser13@gmail.com>
2019-09-21 19:21:34 +0300

    mainProjectLesson Practic add calculator

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -278,8 +278,8 @@ window.addEventListener('DOMContentLoaded', function () {
     command();
     //#endregion
 
-    //#region Calc
-    const calc = () => {
+    //#region CheckCalc
+    const checkCalc = () => {
         const inputs = document.querySelectorAll('.calck-block input');
         inputs.forEach((elem) => {
             elem.addEventListener('input', (e) => {
@@ -288,7 +288,55 @@ window.addEventListener('DOMContentLoaded', function () {
         });
 
     }
-    calc();
+    checkCalc();
+    //#endregion
+
+    //#region Calculator
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            caclCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const coutnSum = () => {
+            let total = 0,
+                dayValue = 1,
+                countValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (caclCount.value > 1) {
+                countValue += (caclCount.value - 1) / 10;
+            }
+
+            if (calcDay && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')) {
+                coutnSum();
+            }
+        });
+
+
+    };
+
+    calc(100);
     //#endregion
 
     function scrollTo(event) {
